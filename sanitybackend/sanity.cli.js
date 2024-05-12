60914d538edc210697f770cff0d29d1380b18364
Najib Bouabdallah <119747481+NajibBouabdallah@users.noreply.github.com>
2024-05-12 15:34:13 +0100

    Delete sanitybackend directory

--- sanitybackend/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'vff6qtex',
-    dataset: 'production'
-  }
-})
--- sanitybackend/schemaTypes/index.js
+++
@@ -1,7 +0,0 @@
-import user from "./user"
-import Router from "./Router"
-
-
-export const schemaTypes = [
-    user,Router
-]
